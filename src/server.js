22b56c83f6503aa76303a2d2a0d5117dec4c929a
Mawais-10 <fa23-bcs-061@cuiatk.edu.pk>
2026-01-30 15:28:18 +0500

    implement prometheus

--- src/server.js
+++ src/server.js
@@ -2,11 +2,38 @@ require("dotenv").config()
 const userroutes = require("./routes/userroute")
 const taskroutes = require("./routes/Taskroutes")
 
+const promclient = require("prom-client")
+
+
+
 const express = require("express")
 const app = express()
 app.use(express.json())
 
+
+const register = new promclient.Registry()
+
+promclient.collectDefaultMetrics({ register })
+
+
+
+const httpCounterReq = new promclient.Counter({
+    name: "http_requests_total",
+    help: "Total number of HTTP requests",
+    labelNames: ["method", "route", "status"]
+})
+
+
+register.registerMetric(httpCounterReq)
+
 const PORT = process.env.PORT || 3000
+
+app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", register.contentType)
+    res.end(await register.metrics())
+})
+
+
 
 app.use('/api/adduser', userroutes)
 app.use('/api/addtask', taskroutes)
